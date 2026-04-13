b18694f1543d92b0e419d26d4644f93b021595e0
lizadvdv06-crypto <lizadvdv06@gmail.com>
2026-04-13 11:41:18 +0400

    Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <string>
+#include <omp.h>
 
 using namespace std;
-int main() {
-    ifstream fa("C:\\Users\\user\\Desktop\\учеба\\A.txt");
-    ifstream fb("C:\\Users\\user\\Desktop\\учеба\\B.txt");
-    ofstream fc("C:\\Users\\user\\Desktop\\учеба\\C.txt");
 
-    int n,m;
-    fa >> n;
-    fb >> m;
-    if (n != m) {
-        cout << "Matrix sizes not equal" << endl;
+int main() {
+    string fileName;
+    int threads;
+
+    cout << "Enter file name (e.g., A.txt, B.txt...): ";
+    cin >> fileName;
+    cout << "Enter number of threads: ";
+    cin >> threads;
+
+    // Путь к папке (измени, если папка другая)
+    string path = "C:\\Users\\user\\Desktop\\учеба\\" + fileName;
+    ifstream fa(path);
+
+    if (!fa.is_open()) {
+        cout << "File not found!" << endl;
         return 1;
     }
 
+    int n;
+    fa >> n; // Считываем размер матрицы из файла
+
+    // Выделяем память под две матрицы (будем множить матрицу саму на себя для теста скорости)
+    // или если у тебя в файле две матрицы, считай вторую так же.
     int** A = new int* [n];
     int** B = new int* [n];
     int** C = new int* [n];
@@ -26,16 +39,22 @@ int main() {
         C[i] = new int[n];
     }
 
+    // Заполняем матрицу A данными из файла
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             fa >> A[i][j];
 
+    // Для простоты эксперимента сделаем B такой же как A
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            fb >> B[i][j];
+            B[i][j] = A[i][j];
+
+    // Установка потоков
+    omp_set_num_threads(threads);
 
     auto start = chrono::high_resolution_clock::now();
 
+#pragma omp parallel for
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             C[i][j] = 0;
@@ -48,18 +67,16 @@ int main() {
     auto end = chrono::high_resolution_clock::now();
     double time = chrono::duration<double>(end - start).count();
 
-    fc << n << endl;
+    cout << "-----------------------------------" << endl;
+    cout << "File: " << fileName << " (Size: " << n << "x" << n << ")" << endl;
+    cout << "Threads used: " << threads << endl;
+    cout << "Time: " << time << " seconds" << endl;
+    cout << "-----------------------------------" << endl;
+
+    // Очистка памяти (чтобы препод не ругался)
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            fc << C[i][j] << " ";
-        }
-        fc << endl;
+        delete[] A[i]; delete[] B[i]; delete[] C[i];
     }
-
-    cout << "Time: " << time << " sec" << endl;
-
-    long long ops = (long long)n * n * n;
-    cout << "Task size (operations): " << ops << endl;
+    delete[] A; delete[] B; delete[] C;
 
     return 0;
-}
